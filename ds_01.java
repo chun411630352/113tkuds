481707e7f37e1eec0d41a9746528650586c41167
chun411630352 <riley20040305@gmail.com>
2025-06-26 13:47:59 +0800

    Add files via upload

---
+++ ds_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ds_01 {
+    public static void main(String[] args) {
+        Scanner cin =new Scanner(System.in);
+            int a,b;
+            a=cin.nextInt();
+            b=cin.nextInt();
+            System.out.println(a+b);
+        }
+            
+    }
+    
---
+++ ds_04.java
@@ -0,0 +1,9 @@
+
+import java.util.Arrays;
+
+public class ds_04 {
+    public static void main(String[] args) {
+     int[] a={35,79,51,99,7} ;
+     System.out.print(Arrays.toString((a)));
+    }
+}
